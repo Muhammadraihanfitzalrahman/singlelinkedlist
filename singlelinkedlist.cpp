ae73091eeacdb1964437e18736c3633bcceae1b9
muhammadraihanfitzalrahman <Ayamtulebo19@gmail.com>
2025-05-08 21:02:30 +0700

    Node previous

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -32,3 +32,16 @@ public:
             if ((START != NULL) && (nim == START->noMhs))
             return;
         }
+        Node *previous = START;
+        Node *current = START;
+
+        while ((current != NULL) && (nim >= current->noMhs))
+    }
+         if (nim == current->noMhs)
+         {
+            cout << "\nDuplikasi noMhs tidak diijinkan\n";
+            return;
+         }
+         previous = current;
+         current = current->next;
+        }
